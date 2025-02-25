24f97f1f37639da71bf8ff56f07882b961ee5f20
Kathleen Rodrigues <kathleenrorigues2001.29@gmail.com>
2025-02-25 14:23:01 -0300

    teste

---
+++ API/Consulta.cs
@@ -0,0 +1,18 @@
+using System;
+
+class ColsultaMongo {
+    public int Id;
+
+    ColsultaMongo(int id) {
+        Id = id;
+    }
+
+      public static void Consulta(String[] args) {
+        Console.WriteLine("Hello World!");
+    }
+
+}
+
+//classe main com helloword
+
+  
--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca4fe7fea5c356edb37163645616fd58f742d81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed44ea4575f89b59ab8780da11a17f74823292a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
